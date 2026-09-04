e83b17f1faa55be889f4fc734f81e8ee34a9f09b
unknown <tegaguru@gmail.com>
2026-09-04 01:02:30 +0100

    add column after outline icon

--- src/data/mappings.ts
+++ src/data/mappings.ts
@@ -1,9 +1,11 @@
 import {
+  ColumnAfterOutline,
   AppleFullOutline,
   BellOutline,
 } from '../icons/All';
 
 export const iconsMap: Record<string, string> = {
+  'column-after-outline': ColumnAfterOutline,
   'apple-full-outline': AppleFullOutline,
   'bell-outline': BellOutline,
 };
--- src/icons/All.tsx
+++ src/icons/All.tsx
@@ -1,7 +1,9 @@
+import ColumnAfterOutline from './ColumnAfterOutline';
 import AppleFullOutline from './AppleFullOutline';
 import BellOutline from './BellOutline';
 
 export {
+  ColumnAfterOutline,
   AppleFullOutline,
   BellOutline,
 };
