6b7bb5fbdc2a96ffe59108bf067ded853ad9c95f
unknown <tegaguru@gmail.com>
2026-09-05 21:29:25 +0100

    add address book outline icon

--- src/icons/All.tsx
+++ src/icons/All.tsx
@@ -1,8 +1,10 @@
+import AddressBookOutline from './AddressBookOutline';
 import ColumnAfterOutline from './ColumnAfterOutline';
 import AppleFullOutline from './AppleFullOutline';
 import BellOutline from './BellOutline';
 
 export {
+  AddressBookOutline,
   ColumnAfterOutline,
   AppleFullOutline,
   BellOutline,
