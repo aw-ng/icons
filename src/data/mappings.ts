1a0a847e5987e938fcc934cf1bfb3e8e729e4442
badasswp <badasswpdev@gmail.com>
2026-09-07 11:29:32 +0100

    chore: to show an example

--- src/data/mappings.ts
+++ src/data/mappings.ts
@@ -5,6 +5,7 @@ import {
 } from '../icons/All';
 
 export const iconsMap: Record<string, string> = {
+  'example-to-show-git': AddColumnAfterOutline,
   'add-column-after-outline': AddColumnAfterOutline,
   'apple-full-outline': AppleFullOutline,
   'bell-outline': BellOutline,
